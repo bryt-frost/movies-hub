f41181dfec82e18e0ede8ec5b8282638da3d3a12
brytfrost <brytfrost@gmail.com>
2024-05-15 23:30:37 +0000

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
-import { ThemeProvider } from '@/components/ThemeToggler';
+import { ThemeProvider } from '@/components/ThemeProvider';
 import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
